60c978a727fa9d5368b8585906ab6fa608a2fe99
DRSJir <jairdelreal@gmail.com>
2026-03-28 13:27:27 -0600

    status del api

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def hello_world():
     return {"mensaje": "Hola mundo!"}
 
+@app.route('/api/status', methods=["GET"])
+def check_status():
+    return jsonify({
+        "status": "online",
+        "project": "AXIS Precision Tools API",
+        "verison": "1.0"
+    }), 200
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
